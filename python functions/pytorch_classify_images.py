3bbba7d46cdc2eeb22ff2743dc081372d30deb5b
SasikaA073 <sasikapamith2016@gmail.com>
2024-09-23 02:15:56 +0530

    Add sample coordinates.json

--- python functions/pytorch_classify_images.py
+++ python functions/pytorch_classify_images.py
@@ -44,8 +44,8 @@ class ImageClassifier:
             self.labels = json.load(f)
 
         self.preprocess = transforms.Compose([
-            transforms.Resize(299),
-            transforms.CenterCrop(299),
+            transforms.Resize(224),
+            # transforms.CenterCrop(299),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
